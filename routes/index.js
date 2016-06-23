9a017732359cfe54df23068dfb816e09ef1ba1c5
Raedslab <raed@riseup.net>
2016-06-23 09:59:04 +0200

    lots

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,37 @@ router.get('/', function (req, res, next) {
     var ciphertext = "";
     if (plaintext === undefined || plaintext.length < 1) {
         errorMessage = "Data input must not be empty. Pass ?data=XYZ param.";
+        plaintext = ciphertext = "ERROR";
+    } else if (plaintext.length > 16) {
+        errorMessage = "Input can't be more than 128-bits";
+        plaintext = ciphertext = "ERROR";
     } else {
         plaintext = toHex(plaintext).toUpperCase();
         ciphertext = encrypt(plaintext);
     }
-
     res.render('index', {plaintext: plaintext, error: errorMessage, ciphertext: ciphertext});
 });
 
+router.get('/xor', function (req, res, next) {
+    var input1 = req.param('a');
+    var input2 = req.param('b');
+    var xorres = "";
+    var error = "None";
+    if ((input1 === undefined || input1.length < 1) || (input2 === undefined || input2.length < 1)) {
+        xorres = "ERROR";
+        error = "Both inputs (a,b) are required (?a=XYZ&b=XYZ)";
+    } else {
+        var input1Bytes = forge.util.hexToBytes(input1);
+        var input2Bytes = forge.util.hexToBytes(input2);
+        var len = Math.max(input1Bytes.length, input2Bytes.length);
+        xorres = forge.util.xorBytes(input1Bytes, input2Bytes, len);
+        xorres = forge.util.bytesToHex(xorres).toUpperCase();
+
+    }
+
+    res.render('xor', {input1: input1, input2: input2, xorres: xorres, error: error});
+})
+;
 
 function toHex(str) {
     var hex = '';
@@ -27,20 +50,20 @@ function toHex(str) {
 }
 
 function encrypt(messageHEX) {
-    var key = forge.util.hexToBytes("01F9BACA9F8C716D5F22D8D427B2F5B7");
-    var iv = forge.util.hexToBytes("00000000000000000000000000000000");
+
+    var KeyHex = process.env.SECRET_KEY;
+    var key = forge.util.hexToBytes(KeyHex);
+
+    var nonce = forge.util.hexToBytes("00000000000000000000000000000000");
 
     var messageBytes = forge.util.hexToBytes(messageHEX);
-
     var messageBuffer = forge.util.createBuffer(messageBytes);
 
     var cipher = forge.cipher.createCipher('AES-CTR', key);
-    cipher.start({iv: iv});
+    cipher.start({iv: nonce});
     cipher.update(messageBuffer);
     cipher.finish();
-
     return cipher.output.toHex().toUpperCase();
-
 }
 
 module.exports = router;
